1106f106c260c1ea0da56b942944462a9cdeaf3f
d-shubh <110290476+d-shubh@users.noreply.github.com>
2024-07-18 15:54:12 +0530

    add 🍪biscuit colorscheme; modify nvim config

--- .config/nvim/lua/plugins/user-plugins.lua
+++ .config/nvim/lua/plugins/user-plugins.lua
@@ -1,26 +1,41 @@
 return {
-  {
-    "kevinhwang91/nvim-ufo",
-    dependencies = "kevinhwang91/promise-async",
-    opts = {},
-  }, -- nvim-ufo {for folded lines}
-  { "Pocco81/auto-save.nvim" }, -- autosave
-  { "norcalli/nvim-colorizer.lua" }, -- color highlighter,
-  {
-    "numToStr/Comment.nvim",
-    opts = {},
-    lazy = false,
-  },
-  {
-    "elkowar/yuck.vim",
-  },
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function()
-      vim.fn["mkdp#util#install"]()
-    end,
-  },
+	{
+		"kevinhwang91/nvim-ufo",
+		dependencies = "kevinhwang91/promise-async",
+		opts = {},
+	}, -- nvim-ufo {for folded lines}
 
+	{
+		"okuuva/auto-save.nvim",
+		cmd = "ASToggle", -- optional for lazy loading on command
+		event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+		opts = {
+			enabled = true,
+			execution_message = {
+				enabled = false,
+				message = function() -- message to print on save
+					return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
+				end,
+				dim = 0.18, -- dim the color of `message`
+				cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
+			},
+		},
+	}, -- autosave
+	{ "norcalli/nvim-colorizer.lua" }, -- color highlighter,
+	{
+		"numToStr/Comment.nvim",
+		opts = {},
+		lazy = false,
+	},
+	{
+		"elkowar/yuck.vim",
+	},
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = { "markdown" },
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	},
 }
